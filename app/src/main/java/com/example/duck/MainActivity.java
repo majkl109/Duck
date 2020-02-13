173df6006251b2bb10764db1ff6ef72e169174c9
Majkl <majkl.andracic@gmail.com>
2020-02-13 23:47:58 +0100

    Switch

--- app/src/main/java/com/example/duck/MainActivity.java
+++ app/src/main/java/com/example/duck/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.duck;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.ShowableListMenu;
 
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,14 +18,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onToggleButtonClicked(View view){
 
-        boolean on = ((ToggleButton)view).isChecked();
-            if(on){
-                System.out.println("Button is clicked");
-            }else{
-                System.out.println("Button is not clicked");
+
+
+    public void onToggleButtonClicked(View view) {
+
+        boolean on = ((ToggleButton) view).isChecked();
+        if (on) {
+
+            System.out.print("hokey");
+
+        } else {
+
+            System.out.print("not ok");
 
         }
     }
+
+    public void onSwitchClicked(View view) {
+
+        boolean on = ((Switch)view).isChecked();
+
+        if(on){
+            //on
+        }else{
+            //off
+        }
+    }
+
+
 }
