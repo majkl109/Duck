cda2c239875e7c3172deb473a4c14a2e45fa39e9
Majkl <majkl.andracic@gmail.com>
2020-02-17 23:33:16 +0100

    RVAdapter class

--- app/src/main/java/MyRecycleViewAdapter.java
+++ app/src/main/java/MyRecycleViewAdapter.java
@@ -1,8 +1,13 @@
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.duck.R;
 
 import java.util.List;
 
@@ -15,5 +20,11 @@ public class MyRecycleViewAdapter extends RecyclerView.Adapter<MyRecycleViewAdap
     MyRecycleViewAdapter(Context context, List<String> data){
      this.mInflater = LayoutInflater.from(context);
      this.mData = data;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View view = mInflater.inflate(R.layout.recycleview_row,parent,false);
+        return new ViewHolder(view);
     }
 }
