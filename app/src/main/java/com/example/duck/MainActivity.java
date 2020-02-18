587881adf253fc32d25a54e3647c651a6a2c688f
Majkl <majkl.andracic@gmail.com>
2020-02-18 23:34:55 +0100

    Start

--- app/src/main/java/com/example/duck/MainActivity.java
+++ app/src/main/java/com/example/duck/MainActivity.java
@@ -1,18 +1,11 @@
 package com.example.duck;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.view.menu.ShowableListMenu;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
-import android.view.Display;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.Switch;
-import android.widget.ToggleButton;
-
+import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements MyRecycleViewAdapter.ItemClickListener{
@@ -36,43 +29,14 @@ public class MainActivity extends AppCompatActivity implements MyRecycleViewAdap
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new MyRecycleViewAdapter(this, animalNames);
         adapter.setClickListener(this);
-        recyclerView.setAdapter();
+        recyclerView.setAdapter(adapter);
 
     }
-    CheckBox checkbox = findViewById(R.id.checkbox_yellow);
-    boolean checked = checkbox.isChecked();
 
-    if (checked){
-        //do something
+    @Override
+    public void onItemClick(View view, int position){
+        Toast.makeText(this, "You clicked " + adapter.getItem(position)
+                + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
-
-
-
-    public void onToggleButtonClicked(View view) {
-
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-
-            System.out.print("hokey");
-
-        } else {
-
-            System.out.print("not ok");
-
-        }
-    }
-
-    public void onSwitchClicked(View view) {
-
-        boolean on = ((Switch)view).isChecked();
-
-        if(on){
-            //on
-        }else{
-            //off
-        }
-    }
-
-
 
 }
